439bd4011e1395ca197753758b30cd634e724db5
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-01-11 15:03:32 -0700

    created user component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,77 @@
 import React from 'react';
+import axios from 'axios';
+import User from './components/User';
+import './App.css';
 
 class App extends React.Component {
+  state = {
+    githubUser: null,
+    username: '',
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users/lizzythomson`)
+      .then((response) => {
+        this.setState({ ...this.state, githubUser: response.data });
+      })
+      .catch((error) => {
+        console.log('Error: ', error);
+      });
+  }
+
+  handleChange = (event) => {
+    this.setState({ ...this.state, username: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const username = this.state.username;
+    axios
+      .get(`https://api.github.com/users/${username}`)
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.log('Error: ', error);
+      });
+  };
+
   render() {
     return (
       <div>
         <div className='header'>
           <h1>GitHub Info</h1>
-          <form>
-            <input />
+          <form onSubmit={this.handleSubmit}>
+            <input value={this.state.username} onChange={this.handleChange} />
             <button className='search-btn'>Search</button>
           </form>
-          <div className='main-content'>
-            <div className='github-account'>
-              <img></img>
-              <h2>Name</h2>
-              <p>github username</p>
-              <h3>Total Repos</h3>
-              <h3>Total Followers</h3>
-            </div>
-            <div className='followers'>
-              <h2>Followers</h2>
-              <div className='follower'>
-                <img></img>
-                <p>Follower Name</p>
-              </div>
-              <div className='follower'>
-                <img></img>
-                <p>Follower Name</p>
-              </div>
-              <div className='follower'>
-                <img></img>
-                <p>Follower Name</p>
-              </div>
-              <div className='follower'>
-                <img></img>
-                <p>Follower Name</p>
+          {this.state.githubUser === null ? (
+            <h1>Loading...</h1>
+          ) : (
+            <div className='main-content'>
+              <User githubUser={this.state.githubUser} />
+              <div className='followers'>
+                <h2>Followers</h2>
+                <div className='follower'>
+                  <img></img>
+                  <p>Follower Name</p>
+                </div>
+                <div className='follower'>
+                  <img></img>
+                  <p>Follower Name</p>
+                </div>
+                <div className='follower'>
+                  <img></img>
+                  <p>Follower Name</p>
+                </div>
+                <div className='follower'>
+                  <img></img>
+                  <p>Follower Name</p>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     );
---
+++ src/components/User.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class User extends React.Component {
+  render() {
+    const { avatar_url, name, login, followers, public_repos } =
+      this.props.githubUser;
+    return (
+      <div className='github-account'>
+        <img width='200' src={avatar_url}></img>
+        <div className='github-info'>
+          <h2>{name}</h2>
+          <p>({login})</p>
+          <h3>Total Repos: {public_repos}</h3>
+          <h3>Total Followers: {followers}</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default User;
