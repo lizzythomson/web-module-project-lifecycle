108be97c4d81c343233c87b0a721d08e0b1b7c2f
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-01-11 14:02:13 -0700

    created basic DOM layout

--- src/App.js
+++ src/App.js
@@ -2,9 +2,45 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return (
+      <div>
+        <div className='header'>
+          <h1>GitHub Info</h1>
+          <form>
+            <input />
+            <button className='search-btn'>Search</button>
+          </form>
+          <div className='main-content'>
+            <div className='github-account'>
+              <img></img>
+              <h2>Name</h2>
+              <p>github username</p>
+              <h3>Total Repos</h3>
+              <h3>Total Followers</h3>
+            </div>
+            <div className='followers'>
+              <h2>Followers</h2>
+              <div className='follower'>
+                <img></img>
+                <p>Follower Name</p>
+              </div>
+              <div className='follower'>
+                <img></img>
+                <p>Follower Name</p>
+              </div>
+              <div className='follower'>
+                <img></img>
+                <p>Follower Name</p>
+              </div>
+              <div className='follower'>
+                <img></img>
+                <p>Follower Name</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
